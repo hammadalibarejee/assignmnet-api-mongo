38099c7a9009e335e15bca0af305d7bccf4e9eb5
Hammad Ali <34187587+hammadalibarejee@users.noreply.github.com>
2021-11-10 17:15:28 +0500

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -1,4 +1,31 @@
 import express from "express";
 import cors from "corse";
 import mongoose from "mongoose";
-mongoose.connect("");
+import { json } from "stream/consumers";
+mongoose.connect("mongodb+srv://hammadali:hammad123@cluster0.gttlm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
+
+const User = mongoose.model("User",{
+    name:String,
+    email:String,
+    address:String
+});
+const app=express();
+const port= process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express(json));
+
+app.get("/",(req,res)=>{
+    res.send("Hello World")
+});
+
+app.get("/users",(req,res)=>{
+    User.find({},(err,users)=>{
+        if(!err){
+            res.send(users)
+        }
+        else{
+            res.status(500).send("Error Occured!")
+        }
+    })
+})
